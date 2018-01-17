d6446e41c31796d404c161b2220bf5e25121e189
David Fok <david_fok@hotmail.com>
2018-01-17 22:38:46 +0000

    added character-positioning

---
+++ character-positioning.js
@@ -0,0 +1,16 @@
+var countLetters = function letterCounting(input) {
+  var result = {};
+
+  for (var i = 0; i < input.length; ++i) {
+    var letter = input[i];
+    if (letter !== " ") {
+      if (!result[letter]) {
+        result[letter] = []
+      }
+      result[letter].push(i);
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
