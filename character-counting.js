ed0ca0ab6108c734fdee7fbcf49cf7afe52e8ecf
David Fok <david_fok@hotmail.com>
2018-01-17 21:27:21 +0000

    added

---
+++ character-counting.js
@@ -0,0 +1,16 @@
+var countLetters = function letterCounting(input) {
+  var result = {};
+
+  for (var i = 0; i < input.length; ++i) {
+    var letter = input[i];
+    if (letter !== " ") {
+      if (!result[letter]) {
+        result[letter] = 0
+      }
+    result[letter] = result[letter] + 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
